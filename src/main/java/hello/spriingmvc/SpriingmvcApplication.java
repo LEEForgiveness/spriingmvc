57c3f6a8dc4773a840f58f4543e2581d493e16d2
grang <leesln1@naver.com>
2024-01-16 03:01:19 +0900

    Initial commit

---
+++ src/main/java/hello/spriingmvc/SpriingmvcApplication.java
@@ -0,0 +1,13 @@
+package hello.spriingmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpriingmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpriingmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/spriingmvc/SpriingmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spriingmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpriingmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
